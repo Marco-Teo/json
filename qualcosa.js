acbad26d5f2c167c125df8ea4d7aca5f27f18720
Marco Teodoro Mancuso (ext.) <95038449+Marco-Teo@users.noreply.github.com>
2025-03-18 16:28:15 +0100

    first push

---
+++ qualcosa.js
@@ -0,0 +1,27 @@
+let nameArray =[]
+const inputName = document.getElementById('name')
+const saveButton = document.getElementById('save')
+const deleateButton = document.getElementById('deleate')
+const form = document.getElementById('name-form')
+
+
+
+class Name {
+    constructor(_name) {
+      this.name = _name
+    }
+  }
+saveButton.addEventListener('click', function (e) {
+    e.preventDefault()
+    const name = inputName.value 
+    const nameSaved = new Name(name)
+    console.log('nameSaved', nameSaved)
+    nameArray.push(nameSaved)
+
+    sessionStorage.setItem("name",nameArray)
+    form.reset()
+})
+deleateButton.addEventListener('click', function (e) {
+    nameArray.pop()
+    sessionStorage.removeItem("name")
+})
